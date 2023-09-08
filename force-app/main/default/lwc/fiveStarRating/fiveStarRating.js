33c00d94fc64b7a9deb461cb9f6aa754b6ddc509
Rodrigo Soares <rodrigo.soaresvalente@ibm.com>
2023-09-08 15:42:13 -0300

    fiveStarRating component - part 02 - fiveStarRating.js file - get starClass method

--- force-app/main/default/lwc/fiveStarRating/fiveStarRating.js
+++ force-app/main/default/lwc/fiveStarRating/fiveStarRating.js
@@ -8,4 +8,16 @@ const ERROR_VARIANT = 'error';
 const EDITABLE_CLASS = 'c-rating';
 const READ_ONLY_CLASS = 'readonly c-rating';
 
-export default class FiveStarRating extends LightningElement {}
+export default class FiveStarRating extends LightningElement {
+    @api
+    readOnly;
+    @api
+    value;
+  
+    editedValue;
+    isRendered;
+
+    get starClass() {
+        return this.readOnly ? READ_ONLY_CLASS : EDITABLE_CLASS;
+      }
+}
