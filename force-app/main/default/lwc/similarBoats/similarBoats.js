5f608e0adf4e45049013b0e42af743415c778994
Rodrigo Soares <rodrigo.soaresvalente@ibm.com>
2023-09-08 16:57:58 -0300

    similarBoats component - part 02 - similarBoats.js file - get and set recordId methods

--- force-app/main/default/lwc/similarBoats/similarBoats.js
+++ force-app/main/default/lwc/similarBoats/similarBoats.js
@@ -1,3 +1,25 @@
-import { LightningElement } from 'lwc';
+import { api, LightningElement, wire } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation'
+import getSimilarBoats from '@salesforce/apex/BoatDataService.getSimilarBoats';
 
-export default class SimilarBoats extends LightningElement {}
+const BOAT_OBJECT = 'Boat__c';
+
+export default class SimilarBoats extends NavigationMixin(LightningElement) {
+
+    currentBoat;
+    relatedBoats;
+    boatId;
+    error;
+
+    @api
+    get recordId() {
+        return this.boatId;
+    }
+    set recordId(value) {
+        this.setAttribute('boatId', value);        
+        this.boatId = value;
+    }
+    
+    @api
+    similarBy;
+}
