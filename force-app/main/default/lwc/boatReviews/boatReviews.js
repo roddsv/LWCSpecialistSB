ac9918cf295d751b746490a3162fcc27db1d6fef
Rodrigo Soares <rodrigo.soaresvalente@ibm.com>
2023-09-08 16:24:51 -0300

    boatReviews component - part 02 - boatReviews.js file - getReviewsToShow method

--- force-app/main/default/lwc/boatReviews/boatReviews.js
+++ force-app/main/default/lwc/boatReviews/boatReviews.js
@@ -11,11 +11,15 @@ export default class BoatReviews extends NavigationMixin(LightningElement) {
     get recordId() {
         return this.boatId;
     }
-    
+
     @api
     set recordId(value) {
         this.setAttribute('boatId', value);        
         this.boatId = value;      
         this.getReviews();
+    }
+
+    get reviewsToShow() {
+        return this.boatReviews !== undefined && this.boatReviews != null && this.boatReviews.length > 0;
     }
 }
