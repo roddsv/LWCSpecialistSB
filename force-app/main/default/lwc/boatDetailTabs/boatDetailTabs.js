a27f8ece08927d56569bdc5a3db132b86007888e
Rodrigo Soares <rodrigo.soaresvalente@ibm.com>
2023-09-08 16:33:49 -0300

    boatDetailTabs component - part 02 - boatDetailTabs.js file - imports

--- force-app/main/default/lwc/boatDetailTabs/boatDetailTabs.js
+++ force-app/main/default/lwc/boatDetailTabs/boatDetailTabs.js
@@ -1,3 +1,20 @@
-    import { LightningElement } from 'lwc';
+import { wire, LightningElement } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+
+
+import labelDetails from '@salesforce/label/c.Details';
+import labelReviews from '@salesforce/label/c.Reviews';
+import labelAddReview from '@salesforce/label/c.Add_Review';
+import labelFullDetails from '@salesforce/label/c.Full_Details';
+import labelPleaseSelectABoat from '@salesforce/label/c.Please_select_a_boat';
+
+import BOAT_ID_FIELD from '@salesforce/schema/Boat__c.Id';
+import BOAT_NAME_FIELD from '@salesforce/schema/Boat__c.Name';
+const BOAT_FIELDS = [BOAT_ID_FIELD, BOAT_NAME_FIELD];
+
+import BOATMC from '@salesforce/messageChannel/BoatMessageChannel__c';
+import { subscribe, APPLICATION_SCOPE, MessageContext } from 'lightning/messageService';
 
 export default class BoatDetailTabs extends LightningElement {}
