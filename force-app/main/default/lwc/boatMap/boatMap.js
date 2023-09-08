a19c2913101bdcc23d2f2a83a523929f3aa6515c
Rodrigo Soares <rodrigo.soaresvalente@ibm.com>
2023-09-08 13:38:45 -0300

    boatMap component - part 02 - boatMap.js file - wiredRecord method

--- force-app/main/default/lwc/boatMap/boatMap.js
+++ force-app/main/default/lwc/boatMap/boatMap.js
@@ -1,4 +1,5 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
 import BOATMC from '@salesforce/messageChannel/BoatMessageChannel__c';
 import { subscribe, APPLICATION_SCOPE, MessageContext } from 'lightning/messageService';
 
@@ -20,5 +21,26 @@ export default class BoatMap extends LightningElement {
     setRecordId(value) {
         this.setAttribute('boatId', value);
         this.boatId = value;
+    }
+
+    error = undefined;
+    mapMakers = [];
+
+    @wire(MessageContext)
+    messageContext;
+
+    @wire(getRecord, {recordId: '$boatId', fields: BOAT_FIELDS })
+    
+    wiredRecord({error, data}) {
+        if (data) {
+            this.error = undefined;
+            const longitude = data.fields.Geolocation__Longitude__s.value;
+            const latitude = data.fields.Geolocation__Latitude__s.value;
+            this.uptadeMap(longitude, latitude);
+        } else if (error) {
+            this.error = error;
+            this.boatId = undefined;
+            this.mapMakers = [];
+        }
     }
 }
