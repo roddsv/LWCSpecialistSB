703cc68b402fed5c85e875c1e01c7e92d902c369
Rodrigo Soares <rodrigo.soaresvalente@ibm.com>
2023-09-08 16:24:13 -0300

    boatReviews component - part 02 - boatReviews.js file - get and set recordId method

--- force-app/main/default/lwc/boatReviews/boatReviews.js
+++ force-app/main/default/lwc/boatReviews/boatReviews.js
@@ -1,3 +1,21 @@
-import { LightningElement } from 'lwc';
+import { api, LightningElement } from 'lwc';
+import getAllReviews from '@salesforce/apex/BoatDataService.getAllReviews';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class BoatReviews extends LightningElement {}
+export default class BoatReviews extends NavigationMixin(LightningElement) {
+    boatId;
+    error;
+    boatReviews;
+    isLoading;
+    
+    get recordId() {
+        return this.boatId;
+    }
+    
+    @api
+    set recordId(value) {
+        this.setAttribute('boatId', value);        
+        this.boatId = value;      
+        this.getReviews();
+    }
+}
