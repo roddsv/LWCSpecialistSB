f13f10f65c73f9d2d37c02ec116fa231a36725bb
Rodrigo Soares <rodrigo.soaresvalente@ibm.com>
2023-09-08 15:54:58 -0300

    boatAddReviewForm component - part 01 - boatAddReviewForm.js file - consts and imports

--- force-app/main/default/lwc/boatAddReviewForm/boatAddReviewForm.js
+++ force-app/main/default/lwc/boatAddReviewForm/boatAddReviewForm.js
@@ -1,3 +1,10 @@
-import { LightningElement } from 'lwc';
+import { api, LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import BOAT_REVIEW_OBJECT from '@salesforce/schema/BoatReview__c';
+import NAME_FIELD from '@salesforce/schema/BoatReview__c.Name';
+import COMMENT_FIELD from '@salesforce/schema/BoatReview__c.Comment__c';
+
+const SUCCESS_TITLE = 'Review Created!';
+const SUCCESS_VARIANT = 'success';
 
 export default class BoatAddReviewForm extends LightningElement {}
